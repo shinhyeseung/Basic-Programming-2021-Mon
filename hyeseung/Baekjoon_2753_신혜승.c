bd4b3b8659e86bc85241146c40eff18087a5e812
shinhyeseung <82753773+shinhyeseung@users.noreply.github.com>
2021-05-03 23:27:37 +0900

    Add week 2 homeworks

---
+++ hyeseung/Baekjoon_2753_신혜승.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int year;
+	1 <= year;
+	year <= 4000;
+
+	printf("연도 입력 : ");
+	scanf_s("%d", &year);
+
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+		printf("1");
+	else
+		printf("0");
+
+	return 0;
+}
